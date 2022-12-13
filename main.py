760c93d609f0a71cd7dcd72989795431d2d57771
datadu-de <frank.wagner.1983@gmail.com>
2022-12-13 17:42:12 +0000

    more changes

--- main.py
+++ main.py
@@ -1,106 +1,153 @@
-import csv
-import io
+import os
 import json
+import logging
 import xml.etree.ElementTree as ET
 from pathlib import Path
-
-import logging
 
 import requests
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-XU_BASE_URL = "http://localhost:8065"
-RSD_TEMPLATE = "TEMPLATE.rsd"
-RSD_TARGET_FOLDER = "./OUTPUT"
+required_env_variables = ["XU_BASE_URL", "RSD_TEMPLATE", "RSD_TARGET_FOLDER"]
+
+XU_BASE_URL = os.getenv("XU_BASE_URL")
+RSD_TEMPLATE = os.getenv("RSD_TEMPLATE")
+RSD_TARGET_FOLDER = os.getenv("RSD_TARGET_FOLDER")
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 
+# supoprted datatypes by CData:
+# string, int, double, datetime, and boolean
+#
+# https://cdn.cdata.com/help/DWH/ado/pg_APIinfo.htm#attr
+
+
+TYPE_MAPPING = {
+    "Byte": "int",
+    "Short": "int",
+    "Int": "int",
+    "Long": "int",
+    "Double": "double",
+    "Decimal": "double",
+    "NumericString": "string",
+    "StringLengthMax": "string",
+    "StringLengthUnknown": "string",
+    "ByteArrayLengthExact": "string",
+    "ByteArrayLengthMax": "string",
+    "ByteArrayLengthUnknown": "string",
+    "Date": "datetime",
+    "ConvertedDate": "datetime",
+    "Time": "datetime",
+}
+
+
 def get_extractions():
 
-    res = requests.get(XU_BASE_URL)
-    f = io.StringIO(res.text)
-    extractions = csv.DictReader(f, delimiter=",")
+    meta_url = f"{XU_BASE_URL}/config/extractions/"
+
+    logging.info(f"{meta_url=}")
+
+    res = requests.get(meta_url)
+    content = res.content.decode(res.apparent_encoding)
+    extractions = json.loads(content).get("extractions")
 
     return extractions
 
 
 def get_column_list(extraction_name):
 
-    meta_url = f"{XU_BASE_URL}/metadata/?name={extraction_name}"
+    # meta_url = f"{XU_BASE_URL}/metadata/?name={extraction_name}"
+    # http://localhost:8065/config/extractions/VBAK/result-columns
+    meta_url = (
+        f"{XU_BASE_URL}/config/extractions/{extraction_name}/result-columns"  # NOQA
+    )
 
     res = requests.get(meta_url)
-    f = io.StringIO(res.text)
-    columns = csv.DictReader(f, delimiter=",")
+    content = res.content.decode(res.apparent_encoding)
+    columns = json.loads(content).get("columns")
 
     return columns
 
 
 def generate_rsd(extraction):
-    """
-    sample_extraction = {
-        "Name": "ODP_SalesVolumes",
-        "Type": "ODP",
-        "Source": "ec5",
-        "Destination": "http-json",
-        "LastRun": "",
-        "RowCount": "",
-        "LastChange": "2022-10-27_13:05:22.946",
-        "Created": "2022-10-10_09:55:10.870",
-    }
 
-    sample_column = {
-        "POSITION": "187",
-        "NAME": "PSOO3",
-        "DESC": "Description",
-        "TYPE": "C",
-        "LENGTH": "50",
-        "DECIMALS": "0",
-        "KEY": "false",
-    }
-    """
+    extraction_name = extraction.get("name")
+    target_file_name = Path(RSD_TARGET_FOLDER, extraction_name + ".rsd")
+    extraction_url = (
+        f"{XU_BASE_URL}/?name={extraction_name}" + r"&destination=http-json"
+    )
 
-    target_file_name = Path(RSD_TARGET_FOLDER, extraction["Name"] + ".rsd")
-
-    logging.info(f"{target_file_name=}")
-
-    columns = get_column_list(extraction["Name"])
-
-    tree = ET.parse(RSD_TEMPLATE)
+    # read template RSD
+    template_tree = ET.parse(RSD_TEMPLATE)
 
     namespaces = {
         "api": "http://apiscript.com/ns?v1",
         "xs": "http://www.w3.org/2001/XMLSchema",
+        # "other": "http://apiscript.com/ns?v1",
     }
 
-    # <api:info title="BSEG" desc="Generated schema file." xmlns:other="http://apiscript.com/ns?v1">
-    field_section = tree.find("//api:info", namespaces)
+    for k, v in namespaces.items():
+        ET.register_namespace(k, v)
 
-    field_section.attrib["title"] = extraction["Name"]
+    # set extraction URL
+    template_tree.find("//api:set[@attr='URI']", namespaces=namespaces).attrib[
+        "value"
+    ] = extraction_url
 
-    # <attr name="ZBD2P" xs:type="double" readonly="false" other:xPath="/json/ZBD2P" />
+    # prepare field section to look like this:
+    # <api:info title="BSEG" desc="Generated schema file."
+    #      xmlns:other="http://apiscript.com/ns?v1">
+    field_section = template_tree.find("//api:info", namespaces)
+    field_section.clear()
+    field_section.attrib["title"] = extraction_name
+    field_section.attrib[
+        "desc"
+    ] = f"""Type: {extraction.get("type")}, Source: {extraction.get("source")}"""  # NOQA
+
+    field_section.attrib["xmlns:other"] = "http://apiscript.com/ns?v1"
+
+    columns = get_column_list(extraction_name)
 
     for c in columns:
+
+        attributes = {
+            "name": c.get("name"),
+            "xs:type": TYPE_MAPPING.get(c.get("type"), "unknown"),
+            "key": "true" if c.get("isPrimaryKey") else "false",
+            "other:xPath": f"/json/{c.get('name')}",
+            "readonly": "true",
+        }
+
+        if "length" in c.keys():
+            attributes["columnsize"] = str(c.get("length"))
+
+        if "decimalsCount" in c.keys():
+            attributes["decimaldigits"] = str(c.get("decimalsCount"))
+
+        if "description" in c.keys():
+            attributes["description"] = c.get("description")
+
+        logging.debug(c)
+        logging.debug(attributes)
 
         field_section.append(
             ET.Element(
                 "attr",
-                {
-                    "xs:type": "unknown",
-                    "readonly": "true",
-                    "other:xPath": f"/json/{c['NAME']}",
-                    "columnsize": "100",
-                    "decimaldigits": "0",
-                    "desc": c["DESC"],
-                    "key": "true",
-                },
+                attributes,
             )
         )
 
-        pass
+    # restore xs namespace due to malformed XML structure of RSD format
+    template_tree.getroot().attrib["xmlns:xs"] = namespaces.get("xs")
+
+    target_file_name.parent.mkdir(parents=True, exist_ok=True)
+
+    template_tree.write(
+        target_file_name,
+    )
 
 
 def main():
