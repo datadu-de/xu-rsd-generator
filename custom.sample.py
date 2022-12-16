77fa27861852bc85ce394e107dacb4d8c9376a00
datadu-de <frank.wagner.1983@gmail.com>
2022-12-16 12:03:35 +0100

    added custom.sample.py

---
+++ custom.sample.py
@@ -0,0 +1,14 @@
+from main import FILTER_DESTINATION_TYPE, FORCE_HTTP_JSON_DESTINATION, generate_rsd, get_extractions
+
+
+def main():
+
+    # set filterDestionationType to None to create RSD for *all* extractions
+    extractions = get_extractions(filterDestionationType=FILTER_DESTINATION_TYPE)
+
+    for e in extractions:
+        generate_rsd(e, forceHttpJsonDestination=FORCE_HTTP_JSON_DESTINATION)
+
+
+if __name__ == "__main__":
+    main()
